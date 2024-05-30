f9c6c77131dce0bcfe8e46356c82d6927045a7a1
David Olowomeye <olowomeyedavid242@gmail.com>
2024-05-30 18:08:18 +0100

    Added Prisma Client to show the data in the browser

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import prisma from "@/prisma/db";
 
-const Tickets = () => {
+const Tickets = async () => {
+  const ticketData = await prisma.tickets.findMany();
+
+  console.log("Tickets Data:", ticketData);
+
   return (
     <div>
       <h1>Tickets</h1>
