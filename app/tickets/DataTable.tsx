e0d92dfa2e9a1d26fbc9cdacfefc17a54ba1fc7d
David Olowomeye <olowomeyedavid242@gmail.com>
2024-06-02 20:51:49 +0100

    Made them component contents dynamic

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -1,3 +1,5 @@
+import TicketPriority from "@/components/TicketPriority";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
 import {
   Table,
   TableBody,
@@ -23,8 +25,12 @@ const DataTable = ({ tickets }: Props) => {
             <TableRow>
               <TableHead>S/N</TableHead>
               <TableHead>Title</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Priority</TableHead>
+              <TableHead>
+                <div className="flex justify-center">Status</div>
+              </TableHead>
+              <TableHead>
+                <div className="flex justify-center">Priority</div>
+              </TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
           </TableHeader>
@@ -35,14 +41,25 @@ const DataTable = ({ tickets }: Props) => {
                   <TableRow key={ticket.id} data-href="/">
                     <TableCell>{ticket.id}</TableCell>
                     <TableCell>{ticket.title}</TableCell>
-                    <TableCell>{ticket.status}</TableCell>
-                    <TableCell>{ticket.priority}</TableCell>
+
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketStatusBadge status={ticket.status} />
+                      </div>
+                    </TableCell>
+
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketPriority priority={ticket.priority} />
+                      </div>
+                    </TableCell>
                     <TableCell>
                       {ticket.createdAt.toLocaleDateString("en-US", {
                         year: "2-digit",
                         month: "2-digit",
                         day: "2-digit",
                         hour: "numeric",
+                        minute: "2-digit",
                         hour12: true,
                       })}
                     </TableCell>
