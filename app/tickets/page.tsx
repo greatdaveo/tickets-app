9f55857b756e2f752b7d6423e42024330b3d75cf
David Olowomeye <olowomeyedavid242@gmail.com>
2024-06-03 08:02:54 +0100

    Added Ticket Form and submission

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import prisma from "@/prisma/db";
 import DataTable from "./DataTable";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 const Tickets = async () => {
   const ticketsData = await prisma.tickets.findMany();
 
-  console.log("Tickets Data:", ticketsData);
+  // console.log("Tickets Data:", ticketsData);
 
   return (
     <div>
+      <Link
+        href="/tickets/new"
+        className={buttonVariants({ variant: "default" })}
+      >
+        New Ticket
+      </Link>
       <h1>
         <DataTable tickets={ticketsData} />
       </h1>
